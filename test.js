8cc5848ccd29f48b5a3c5c5c39756f0c99994cee
Anastasia Zaytseva <anastasia.zaytseva0@yandex.ru>
2019-09-29 13:36:18 +0300

    фиксы

--- test.js
+++ test.js
@@ -4,4 +4,26 @@ const getItems = () => [1, 2, 3];
 const fn = (a) => a*2;
 
 const collection = getItems();
-const res = Array.isArray(collection) ? collection.map(fn) : Array.isArray(collection) ? collection.map(fn) : Array.isArray(collection) ? collection.map(fn) : Array.isArray(collection) ? collection.map(fn) : Array.isArray(collection) ? collection.map(fn) : Array.isArray(collection) ? collection.map(fn) : Array.isArray(collection) ? collection.map(fn) : Array.isArray(collection) ? collection.map(fn) : Array.isArray(collection) ? collection.map(fn) : Array.isArray(collection) ? collection.map(fn) : _.map(collection, fn);
+// не должен менять
+const res = Array.isArray(collection) ? collection.map(fn) : _.map(collection, fn);
+
+
+// не должен менять
+function a() {
+    const l = 1;
+    const qw = { l, as: '123' };
+    return _.map(qw, fn);
+}
+
+
+// должно появиться условие
+const secondTern =  Array.isArray(collection) ? collection.map(fn) : _.map(collection, fn);
+
+
+// во втором присваивании не должен менять
+let qwer;
+if(Array.isArray(collection)) {
+    qwer = collection.map(fn);
+} else {
+    qwer = _.map(collection, fn);
+}
